4a1d116ca9aeab3a85ff53a769edc77aa18a302e
bernardo.vitoria@olx.com <bernardo.vitoria@olx.com>
2024-11-27 23:24:17 +0000

    fix: typescript and move changes

--- src/graphql/customer/resolver.ts
+++ src/graphql/customer/resolver.ts
@@ -4,13 +4,11 @@ import customerService from "./datasource";
 const resolvers: IResolvers = {
   Query: {
     customers: async () => {
-      console.log("MUTATION");
       return await customerService.getAllCustomers();
     },
   },
   Mutation: {
     createCustomer: async (_: any, { name }: { name: string }) => {
-      console.log("MUTATION");
       return await customerService.createCustomer({ name });
     },
   },
